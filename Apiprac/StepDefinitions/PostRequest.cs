e8a73f1eb92815992d34b8442fd70946fe55203e
rajeshwari301@gmail.com <rajeshwari301@gmail.com>
2023-07-25 19:56:38 -0400

    Postrequest is implemeneted for step def and support

--- Apiprac/StepDefinitions/PostRequest.cs
+++ Apiprac/StepDefinitions/PostRequest.cs
@@ -1,21 +1,47 @@
+using Apiprac.Support;
+using Newtonsoft.Json;
 using System;
 using TechTalk.SpecFlow;
+using TechTalk.SpecFlow.Infrastructure;
 
 namespace Apiprac.StepDefinitions
 {
     [Binding]
     public class PostRequest
-    {
-        [Given(@"I send a request with posturl ""([^""]*)""")]
-        public void GivenISendARequestWithPosturl(string p0)
+    { 
+        HttpClient httpClient;
+        HttpResponseMessage httpResponseMessage;
+        string responsebody;
+        private readonly ISpecFlowOutputHelper outputHelper;
+
+        public PostRequest(ISpecFlowOutputHelper specFlowOutputHelper)
         {
-            
+            httpClient = new HttpClient();
+            this.outputHelper = specFlowOutputHelper;
+        }
+
+
+    
+        [Given(@"I send a request with posturl ""([^""]*)""")]
+        public async Task GivenISendARequestWithPosturl(string url)
+        {
+            postdata postdata = new postdata()
+            {
+                name = "raji",
+                job = "coder"
+            };
+
+            string data = JsonConvert.SerializeObject(postdata);
+            var contentdata = new StringContent(data);
+            httpResponseMessage = await httpClient.PostAsync(url, contentdata);
+           responsebody = await httpResponseMessage.Content.ReadAsStringAsync();
+            outputHelper.WriteLine("postresponse is"+responsebody);
         }
 
         [Then(@"The request succeeds with status (.*)")]
         public void ThenTheRequestSucceedsWithStatus(int p0)
         {
-            throw new PendingStepException();
+            Assert.True(httpResponseMessage.IsSuccessStatusCode);
         }
 
     }
---
+++ Apiprac/Support/postdata.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Apiprac.Support
+{
+    public class postdata
+    {
+        public string? name { get; set; }
+        public string? job { get; set; }
+    }
+}
